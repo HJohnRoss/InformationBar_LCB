1d4d8e649802a03f15deff0ca426b7fc65bb1050
John Ross <rossjohn936@gmail.com>
2025-01-09 11:31:20 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
+var prevUpdate = null;
+
 function updateText() {
+    console.log("updating")
     const container = document.getElementById("container")
-    while(container.firstChild) {
-        container.firstChild.remove();
-    }
-    // if you make a new file change below to the link
+       // if you make a new file change below to the link
     fetch("https://hjohnross.github.io/InformationBar.txt_LCB/informationBar.txt")
         .then(res => res.text())
         .then(res => {
-            console.log(res)
-            const lines = res.split('\n');
-            for(i = 0; i < lines.length; i++) {
-                container.innerHTML += `<p>${lines[i]}</p>`
+            if(prevUpdate !== res) {
+                while(container.firstChild) {
+                    container.firstChild.remove();
+                }
+
+                const lines = res.split('\n');
+                for(i = 0; i < lines.length; i++) {
+                    container.innerHTML += `<p>${lines[i]}</p>`
+                }
+                prevUpdate = res;
             }
         })
         .catch(err => console.log(err))
