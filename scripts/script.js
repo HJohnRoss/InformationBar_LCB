9c4700ccb04ef533328413aa21e06c4c6cf465d8
John Ross <rossjohn936@gmail.com>
2025-01-09 09:13:09 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,20 @@
-const container = document.getElementById("container")
 function updateText() {
+    const container = document.getElementById("container")
     while(container.firstChild) {
         container.firstChild.remove();
     }
     // if you make a new file change below to the link
-    fetch("https://hjohnross.github.io/Schedule.txt_LCB/infoBar.txt")
+    fetch("https://hjohnross.github.io/InformationBar.txt_LCB/informationBar.txt")
         .then(res => res.text())
         .then(res => {
             console.log(res)
             const lines = res.split('\n');
             for(i = 0; i < lines.length; i++) {
-                container.innerHTML += `<p">${lines[i]}</p>`
+                container.innerHTML += `<p>${lines[i]}</p>`
             }
         })
         .catch(err => console.log(err))
 }
-function removeId() {
-     if(!container.attributes.id) {
-        container.setAttribute("id", "container")
-    } else {
-        container.removeAttribute("id")
-    }
-}
-// updateText();
-// setInterval(updateText, 120000)
-// setInterval(removeId, 120000)
+
+updateText()
+setInterval(updateText, 120000)
