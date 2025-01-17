c9b4bf140360b62015f9aa0dac714ac6e84090be
John Ross <rossjohn936@gmail.com>
2025-01-17 08:58:36 -0800

    test

--- scripts/B10_165.js
+++ scripts/B10_165.js
@@ -1,23 +1,23 @@
-var prevUpdate = null;
+//var prevUpdate = null;
 
 function updateText() {
     const container = document.getElementById("container")
     // if you make a new file change below to the link
     fetch("https://hjohnross.github.io/InformationBar.txt_LCB/rooms/B10_165.txt")
         .then(res => res.text())
-        .then(res => {
-            if(prevUpdate !== res) {
+		.then(res => {
+			//if(prevUpdate !== res) {
                 while(container.firstChild) {
                     container.firstChild.remove();
-                }
+				}
 
                 const lines = res.split('\n');
                 container.innerHTML += `<p>${lines[0]}</p>`
                 prevUpdate = res;
-            }
+            //}
         })
         .catch(err => console.log(err))
 }
 
 updateText()
-//setInterval(updateText, 10000)
+setInterval(updateText, 10000)
