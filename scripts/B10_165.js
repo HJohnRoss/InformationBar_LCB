6dc4c6137ecd2c9e01ef1f87cc0228a9935a8d4f
John Ross <rossjohn936@gmail.com>
2025-01-17 09:52:02 -0800

    update

--- scripts/B10_165.js
+++ scripts/B10_165.js
@@ -1,4 +1,3 @@
-//var prevUpdate = null;
 
 function updateText() {
     const container = document.getElementById("container")
@@ -6,18 +5,14 @@ function updateText() {
     fetch("https://hjohnross.github.io/InformationBar.txt_LCB/rooms/B10_165.txt")
         .then(res => res.text())
 		.then(res => {
-			console.log("updating\n")
-			//if(prevUpdate !== res) {
-                while(container.firstChild) {
-                    container.firstChild.remove();
-				}
+			while(container.firstChild) {
+				container.firstChild.remove();
+			}
 
-                const lines = res.split('\n');
-                container.innerHTML += `<p>${lines[0]}</p>`
-                prevUpdate = res;
-            //}
-        })
-        .catch(err => console.log(err))
+			const lines = res.split('\n');
+			container.innerHTML += `<p>${lines[0]}</p>`
+		})
+		.catch(err => console.log(err))
 }
 
 updateText()
--- scripts/B17_126.js
+++ scripts/B17_126.js
@@ -4,20 +4,17 @@ function updateText() {
     const container = document.getElementById("container")
     // if you make a new file change below to the link
     fetch("https://hjohnross.github.io/InformationBar.txt_LCB/rooms/B17_126.txt")
-        .then(res => res.text())
-        .then(res => {
-            if(prevUpdate !== res) {
-                while(container.firstChild) {
-                    container.firstChild.remove();
-                }
+		.then(res => res.text())
+		.then(res => {
+			while(container.firstChild) {
+				container.firstChild.remove();
+			}
 
-                const lines = res.split('\n');
-                container.innerHTML += `<p>${lines[0]}</p>`
-                prevUpdate = res;
-            }
-        })
-        .catch(err => console.log(err))
+			const lines = res.split('\n');
+			container.innerHTML += `<p>${lines[0]}</p>`
+		})
+		.catch(err => console.log(err))
 }
 
 updateText()
-//setInterval(updateText, 10000)
+setInterval(updateText, 10000)
